cbf90fcc8e779c622eb2a9359eba33c2e6e49d34
jgladch <jeffgladchun@gmail.com>
2014-10-03 19:11:43 -0700

    Stashing my work in a commit, next commit will be Wills repo

--- SQL/db.js
+++ SQL/db.js
@@ -1,5 +1,4 @@
 var mysql = require('mysql');
-var mysql = require('mysql');
 /* If the node mysql module is not found on your system, you may
  * need to do an "sudo npm install -g mysql". */
 
@@ -7,7 +6,7 @@ var mysql = require('mysql');
  * database: "chat" specifies that we're using the database called
  * "chat", which we created by running schema.sql.*/
 var dbConnection = mysql.createConnection({
-  user: "",
+  user: "root",
   password: "",
   database: "chat"
 });
@@ -18,17 +17,38 @@ dbConnection.connect();
  * See https://github.com/felixge/node-mysql for more details about
  * using this module.*/
 
-
-
-
 exports.findAllMessages = function(cb){
+  var queryString = 'SELECT * FROM messages;';
+  dbConnection.query(queryString, function(err, rows){
+    if (err) {console.log(err);}
+    cb(rows);
+  });
 };
 
 exports.findUser = function(username, cb){
+  var queryString = 'SELECT * FROM users WHERE username = ?;';
+  var queryArgs = username;
+  dbConnection.query(queryString, queryArgs, function(err, rows){
+    if (err) {console.log(err)}
+    cb(rows);
+  });
 };
 
 exports.saveUser = function(username, cb){
+  var queryString = "INSERT INTO users (username) VALUES (?);";
+  var queryArgs = username;
+  dbConnection.query(queryString, queryArgs, function(err, rows){
+    if (err) {console.log(err);}
+  });
+  exports.findUser(username, cb);
 };
 
 exports.saveMessage = function(message, userid, roomname, cb){
+  console.log("Save Message: "+message+", "+userid+", "+roomname);
+  var queryString = "INSERT INTO messages (text, user_id, roomname) VALUES (??, ??, ??);";
+  var queryArgs = [message, userid, roomname];
+  dbConnection.query(queryString, queryArgs, function(err, rows){
+    if (err) {console.log(err);}
+    cb(rows);
+  });
 };
